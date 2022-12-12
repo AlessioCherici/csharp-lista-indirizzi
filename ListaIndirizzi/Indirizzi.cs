b30aa9bbed5d6789fa269135af2116dbb61f0342
AlessioCherici <alessio.cherici@hotmail.it>
2022-12-12 17:38:33 +0100

    Creato programma

---
+++ ListaIndirizzi/Indirizzi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+    {
+    public class PersonalData
+        {
+        public string nome;
+        public string cognome;
+        public string street;
+        public string city;
+        public string province;
+        public string zipCode;
+
+        public PersonalData(string nome, string cognome, string street, string city, string province, string zipCode)
+            {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zipCode = zipCode;
+            }
+
+        //-------------------------- Metodo tostring --------------------------
+
+        public override string ToString()
+            {
+            return this.nome + ", " + this.cognome + ", " + this.street + ", " + this.city + ", " +
+            this.province + ", " + this.zipCode;
+            }
+        }
+    }
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using ListaIndirizzi;
+
+List<PersonalData> ListaDati = new List<PersonalData>();
+
+
+
+StreamReader file = File.OpenText("C:\\Users\\aless\\Desktop\\Github\\ListaIndirizzi\\ListaIndirizzi\\Indirizzi.csv");
+
+file.ReadLine();
+
+while (!file.EndOfStream)
+    {
+    string riga = file.ReadLine();
+    string[] infoAddress = riga.Split(",");
+
+
+    string nome = infoAddress[0];
+    while (nome.Length == 0)
+        {
+        Console.WriteLine("Il campo 'nome' è vuoto, inserisci qualcosa!");
+        nome = Console.ReadLine();
+        }
+    string cognome = infoAddress[1];
+    string indirizzo = infoAddress[2];
+    while (indirizzo.Length == 0)
+        {
+        Console.WriteLine("Il campo 'indirizzo' è vuoto, inserisci qualcosa!");
+        indirizzo = Console.ReadLine();
+        }
+    string città = infoAddress[3];
+    string provincia = infoAddress[4];
+    while (provincia.Length == 0)
+        {
+        Console.WriteLine("Il campo 'provincia' è vuoto, inserisci qualcosa!");
+        provincia = Console.ReadLine();
+        }
+    string zipCode = infoAddress[5];
+
+    PersonalData Dati = new PersonalData(nome, cognome, indirizzo, città, provincia, zipCode);
+    ListaDati.Add(Dati);
+    }
+
+
+foreach (PersonalData Dati in ListaDati)
+    {
+    Console.WriteLine(Dati);
+    }
